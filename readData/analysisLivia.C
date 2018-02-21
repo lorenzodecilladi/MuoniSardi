6165612f78f37a0e1a4d85728cb96146bf0cc60b
lorenzodecilladi <lorenzo.decilladi@edu.unito.it>
2018-02-21 16:00:21 +0100

    updated

--- readData/analysisLivia.C
+++ readData/analysisLivia.C
@@ -17,7 +17,7 @@
 #endif
 
 
-void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
+void analysisLivia(TString inputFilePath="readFile.root"){
 
   //open readFile
   TFile   *readFile = new TFile(inputFilePath);
@@ -81,19 +81,25 @@ void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
    
 
    
-   TF1 *fitF1 = new TF1("fitF1","[0]+[1]*TMath::Exp([2]*x) * (-[2]*(1.268+TMath::Exp([2]*x))  -[3]*TMath::Exp([3]*x) )/10.",0.1,1000);
-   fitF1->SetParameter(0, 10.);
-   fitF1->SetParameter(1, 1);
-   fitF1->SetParameter(2, -88);
-   fitF1->SetParameter(3, -10);
-   //[0] = Nbg = backgroung costante
-   //[1] = N0*10/(1+1.268), con N0 = initial particle count
-   //[2] = -10 * lambda0, con lambda0 = costante di decadimento þ[T^-1]
-   //[3] = -10 * lambdaC, con lambdaC = costante di cattura = inverso del tempo di cattura [T^-1]
-   
-   //TF1 *fitF23 = new TF1("fitF1","[0]+294900*10/(1+1.268)*TMath::Exp(-4.036*x/87.88)*((1/87.88)(1.268+TMath::Exp(-4.036*x/87.88))+   *TMath::Exp(-[3]*10*x))",80,4000);
+  //   TF1 *fitF1 = new TF1("fitF1","[0]+[1]*TMath::Exp([2]*x) * (-[2]*(1.268+TMath::Exp([2]*x))  -[3]*TMath::Exp([3]*x) )/10.",0.1,1000);
+  TF1 *fitF1 = new TF1("fitF1","[0]+[1]*TMath::Exp(-[2]*10*x) * ( [2]*(1.268+TMath::Exp(-[2]*10*x)) +[3]*TMath::Exp(-[3]*10*x) )",0,400);
+  fitF1->SetParameter(0, 10.);
+  fitF1->SetParameter(1, 880000000);
+  fitF1->SetParameter(2, 0.01136);
+  fitF1->SetParameter(3, 0.000094);
+  //fitF1->SetParLimits(2, 0, 1);
+  //fitF1->SetParLimits(3, 0, 1);
+  //[0] = Nbg = backgroung costante
+  //[1] = N0*10/(1+1.268), con N0 = initial particle count
+  //[2] = -10 * lambda0, con lambda0 = costante di decadimento þ[T^-1]
+  //[3] = -10 * lambdaC, con lambdaC = costante di cattura = inverso del tempo di cattura [T^-1]
+  
+  //TF1 *fitF23 = new TF1("fitF1","[0]+294900*10/(1+1.268)*TMath::Exp(-4.036*x/87.88)*((1/87.88)(1.268+TMath::Exp(-4.036*x/87.88))+   *TMath::Exp(-[3]*10*x))",80,4000);
 
    TF1 *fitF2 = new TF1("fitF2","10 + (2000000000.*(0.1000/4)/(1+1.268)) * TMath::Exp(-(0.1000/4)*(10*x)/2.2) * ( (1/2.2) * (1.268+TMath::Exp(-(0.1000/4)*(10*x)/2.2))  + 0.00376*TMath::Exp(-0.00376*(0.1000/4)*(10*x)))",0.1,1000);
+
+   
+   
    //TF1 *fitF2 = new TF1("fitF2","100 + (2000000.*1.000/(1+1.268)) * TMath::Exp(-1.000*x/2.2) * ( (1/2.2) * (1.268+TMath::Exp(-1.000*x/2.2))  + 0.0376*TMath::Exp(-0.0376*1.000*x))",0.1,1000);
    TCanvas *cProva = new TCanvas("PROVA", "PROVA", 200, 10, 600, 400);
    cProva->cd();
@@ -104,20 +110,11 @@ void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
      //   for(int j=0; j<16; j++){
      //     histQ->Fill(j,validStr[j]);
      //   }
-     if(!PEDESTAL){//ADCchSG>850 && ADCchSG<2025 && 
-       if(ADCchSG>850 && ADCchSG<2025 && ADCchS1>330 && ADCchS1<2025){
-	 histCLOCK  -> Fill(CLOCKch);
-	 histTIME   -> Fill(TDCch);
-	 histADC1   -> Fill(ADCchS1);
-	 histADCG   -> Fill(ADCchSG);
-       }
-     }
-     else{
-       histCLOCK  -> Fill(CLOCKch);
-       histTIME   -> Fill(TDCch);
-       histADC1   -> Fill(ADCchS1);
-       histADCG   -> Fill(ADCchSG);
-     }
+     histCLOCK  -> Fill(CLOCKch);
+     histTIME   -> Fill(TDCch);
+     histADC1   -> Fill(ADCchS1);
+     histADCG   -> Fill(ADCchSG);
+     
        
      //   histADCsum -> Fill(ADCchS1+ADCchSG);
      //   for(int j=0; j<16; j++){
@@ -138,9 +135,14 @@ void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
    histTIME -> GetXaxis()-> SetTitle("# canali");
    histTIME -> GetYaxis()-> SetTitle("# eventi");
    histTIME -> GetXaxis()->SetRangeUser(0.,1025.);
-   //histTIME -> Fit(fitF1);
-   histTIME -> DrawCopy("hist");
+   histTIME -> Fit(fitF1);
+   histTIME -> DrawCopy("histE");
    fitF2    -> Draw("same");
+
+   cout << "[RESULT   ]" << endl;
+   cout << "[RESULT   ] Muon mean lifetime: (" << (0.1/4)/(fitF1->GetParameter(2)) << "+-" << (1/(fitF1->GetParameter(2)*fitF1->GetParameter(2)))*fitF1->GetParError(2) << ") us" <<endl;
+   cout << "[RESULT   ]" << endl;
+
    
    TCanvas *canvas2 = new TCanvas("CLOCK", "CLOCK", 200, 10, 600, 400);
    //gPad->SetLogy();
@@ -149,14 +151,14 @@ void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
    histCLOCK-> DrawCopy("hist");
    //fitF2    -> Draw("same");
 
-   /*
+   
    TCanvas *canvas3 = new TCanvas("ADC1", "ADC1", 200, 10, 600, 400);
    // gPad->SetLogy();
    histADC1 -> GetXaxis()->SetTitle("# canali");
    histADC1 -> GetYaxis()->SetTitle("# eventi");    
    //histADC1->GetYaxis()->SetRangeUser(0.,3000.);
 
-   //histADC1 -> DrawCopy("hist");
+   histADC1 -> DrawCopy("hist");
    
    
    TCanvas *canvas4 = new TCanvas("ADCG", "ADCG", 200, 10, 600, 400);
@@ -165,8 +167,8 @@ void plot(Bool_t PEDESTAL = kFALSE, TString inputFilePath="readFile.root"){
    histADCG -> GetYaxis()->SetTitle("# eventi");
    //histADCG->GetYaxis()->SetRangeUser(0.,1400.);
    //histADCG->GetXaxis()->SetRangeUser(0.,2025.);  //c'è un picco al canale 2030
-   //histADCG -> DrawCopy("hist");
-   */
+   histADCG -> DrawCopy("hist");
+   
    
    readFile -> Close();
    
